4020250d39b7b3de861ee654670b5b3da5a8e67b
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2024-05-14 16:24:48 -0300

    initial mpi implementation for eye-detector

--- eye-detector/src/mpi.rs
+++ eye-detector/src/mpi.rs
@@ -7,7 +7,11 @@ use opencv::{
     videoio,
 };
 use serde::{de::Visitor, ser::SerializeStruct, Deserialize, Serialize};
-use spar_rust_v2::*;
+
+use spar_rust_v2::mpi::{
+    self,
+    traits::{Communicator, Destination, Source},
+};
 
 #[repr(transparent)]
 #[derive(Debug)]
@@ -124,63 +128,8 @@ struct EyesData {
 unsafe impl Sync for EyesData {}
 unsafe impl Send for EyesData {}
 
-#[source]
-fn source(mut video_in: videoio::VideoCapture) -> impl Iterator<Item = MatData> {
-    (0..)
-        .map(move |_| {
-            let mut frame = Mat::default();
-            video_in.read(&mut frame).unwrap();
-            frame
-        })
-        .take_while(|frame| frame.size().unwrap().width > 0)
-        .map(MatData)
-}
-
-#[stage]
-fn stage1(frame: MatData) -> (MatData, MatData) {
-    let equalized = MatData(common::prepare_frame(&frame.0).unwrap());
-    (frame, equalized)
-}
-
-#[stage(State(face_xml))]
-fn stage2(frame: MatData, equalized: MatData, face_xml: String) -> EyesData {
-    let mut face_detector = objdetect::CascadeClassifier::new(face_xml.clone().as_str()).unwrap();
-    let faces = common::detect_faces(&equalized.0, &mut face_detector).unwrap();
-    EyesData {
-        frame,
-        equalized,
-        faces: unsafe { std::mem::transmute(faces.to_vec()) },
-    }
-}
-
-#[stage(State(eye_xml))]
-fn stage3(eyes_data: EyesData, eye_xml: String) -> MatData {
-    let mut eyes_detector = objdetect::CascadeClassifier::new(eye_xml.clone().as_str()).unwrap();
-    let EyesData {
-        mut frame,
-        equalized,
-        faces,
-    } = eyes_data;
-    for face in faces {
-        let eyes = common::detect_eyes(
-            &core::Mat::roi(&equalized.0, unsafe { std::mem::transmute(face) })
-                .unwrap()
-                .clone_pointee(),
-            &mut eyes_detector,
-        )
-        .unwrap();
-        common::draw_in_frame(&mut frame.0, &eyes, &unsafe { std::mem::transmute(face) }).unwrap();
-    }
-    MatData(frame.0)
-}
-
-#[sink(Ordered)]
-fn sink(frame: MatData) -> MatData {
-    frame
-}
-
 pub fn mpi_eye_tracker(input_video: &String, nthreads: i32) -> opencv::Result<()> {
-    let video_in = videoio::VideoCapture::from_file(input_video, videoio::CAP_FFMPEG)?;
+    let mut video_in = videoio::VideoCapture::from_file(input_video, videoio::CAP_FFMPEG)?;
     let in_opened = videoio::VideoCapture::is_opened(&video_in)?;
     if !in_opened {
         panic!("Unable to open input video {input_video}!");
@@ -201,18 +150,207 @@ pub fn mpi_eye_tracker(input_video: &String, nthreads: i32) -> opencv::Result<()
     let face_xml = core::find_file(unsafe { super::FACE_XML_STR.as_str() }, true, false)?;
     let eye_xml = core::find_file(unsafe { super::EYE_XML_STR.as_str() }, true, false)?;
 
-    let out: Vec<MatData> = to_stream!(mpi: [
-        source(video_in),
-        (stage1(), nthreads as usize ),
-        (stage2(face_xml), nthreads as usize ),
-        (stage3(eye_xml), nthreads as usize ),
-        sink,
-    ])
-    .0
-    .collect();
+    let threads = 1 + nthreads as usize * 3;
+    let (universe, _threading) = mpi::initialize_with_threading(mpi::Threading::Multiple).unwrap();
+    let world = universe.world();
+    let size = world.size() as usize;
 
-    for frame in out {
-        video_out.write(&frame.0).unwrap();
+    if size < threads {
+        panic!("trying to execute with {threads} workers, but only have {size}");
     }
-    Ok(())
+
+    let rank = world.rank();
+
+    if rank as usize >= threads {
+        std::process::exit(unsafe { mpi::ffi::MPI_Finalize() });
+    }
+
+    if rank == 0 {
+        std::thread::spawn(move || {
+            let comm = mpi::topology::SimpleCommunicator::world();
+            let mut target_rank = 1;
+            loop {
+                // Read frame
+                let mut frame = Mat::default();
+                video_in.read(&mut frame).unwrap();
+                if frame.size().unwrap().width == 0 {
+                    break;
+                }
+                let frame = MatData(frame);
+
+                let bytes = bincode::serialize(&frame).unwrap();
+                let size = bytes.len() as u32;
+                let target = comm.process_at_rank(target_rank);
+                target.send(&size.to_ne_bytes());
+                target.send(&bytes);
+
+                target_rank += 1;
+                if target_rank as usize >= threads / 3 {
+                    target_rank = 1;
+                }
+            }
+            for i in 1..(1 + threads / 3) {
+                let target = comm.process_at_rank(i as i32);
+                target.send(&0u32.to_ne_bytes());
+            }
+        });
+
+        let mut out = Vec::new();
+        let mut zeros = threads / 3;
+        let recver = world.any_process();
+        while zeros > 0 {
+            let (size, status) = recver.receive::<u32>();
+            if size == 0 {
+                zeros -= 1;
+                continue;
+            }
+            let mut buf = vec![0u8; size as usize];
+            let _status = world
+                .process_at_rank(status.source_rank())
+                .receive_into(&mut buf);
+            let frame: MatData = bincode::deserialize(&buf).unwrap();
+            out.push(frame);
+        }
+
+        for frame in out {
+            video_out.write(&frame.0).unwrap();
+        }
+
+        return Ok(());
+    } else if rank as usize > 0 && rank as usize <= threads / 3 {
+        let begin = 1 + (threads / 3);
+        let end = 2 * (threads / 3);
+
+        let mut target = begin;
+        let mut zeros = threads / 3;
+        let recver = world.any_process();
+        let sender = mpi::topology::SimpleCommunicator::world();
+        while zeros > 0 {
+            let (size, status) = recver.receive::<u32>();
+            if size == 0 {
+                zeros -= 1;
+                continue;
+            }
+            let mut buf = vec![0u8; size as usize];
+            let _status = world
+                .process_at_rank(status.source_rank())
+                .receive_into(&mut buf);
+
+            let frame: MatData = bincode::deserialize(&buf).unwrap();
+            let equalized = MatData(common::prepare_frame(&frame.0).unwrap());
+
+            {
+                let bytes = bincode::serialize(&(frame, equalized)).unwrap();
+                let size = bytes.len() as u32;
+                let target = sender.process_at_rank(target as i32);
+                target.send(&size.to_ne_bytes());
+                target.send(&bytes);
+            }
+
+            target += 1;
+            if target > end {
+                target = begin;
+            }
+        }
+
+        for target in begin..(end + 1) {
+            let target = sender.process_at_rank(target as i32);
+            target.send(&0u32.to_ne_bytes());
+        }
+    } else if rank as usize > threads / 3 && rank as usize <= 2 * (threads / 3) {
+        let begin = 1 + 2 * (threads / 3);
+        let end = 3 * (threads / 3);
+
+        let mut target = begin;
+        let mut zeros = threads / 3;
+        let recver = world.any_process();
+        let sender = mpi::topology::SimpleCommunicator::world();
+        while zeros > 0 {
+            let (size, status) = recver.receive::<u32>();
+            if size == 0 {
+                zeros -= 1;
+                continue;
+            }
+            let mut buf = vec![0u8; size as usize];
+            let _status = world
+                .process_at_rank(status.source_rank())
+                .receive_into(&mut buf);
+            let (frame, equalized): (MatData, MatData) = bincode::deserialize(&buf).unwrap();
+            let mut face_detector =
+                objdetect::CascadeClassifier::new(face_xml.clone().as_str()).unwrap();
+            let faces = common::detect_faces(&equalized.0, &mut face_detector).unwrap();
+            let eyes_data = EyesData {
+                frame,
+                equalized,
+                faces: unsafe { std::mem::transmute(faces.to_vec()) },
+            };
+
+            {
+                let bytes = bincode::serialize(&eyes_data).unwrap();
+                let size = bytes.len() as u32;
+                let target = sender.process_at_rank(target as i32);
+                target.send(&size.to_ne_bytes());
+                target.send(&bytes);
+            }
+
+            target += 1;
+            if target > end {
+                target = begin;
+            }
+        }
+
+        for target in begin..(end + 1) {
+            let target = sender.process_at_rank(target as i32);
+            target.send(&0u32.to_ne_bytes());
+        }
+    } else {
+        let target = 0;
+        let mut zeros = threads / 3;
+        let recver = world.any_process();
+        let sender = mpi::topology::SimpleCommunicator::world();
+        while zeros > 0 {
+            let (size, status) = recver.receive::<u32>();
+            if size == 0 {
+                zeros -= 1;
+                continue;
+            }
+            let mut buf = vec![0u8; size as usize];
+            let _status = world
+                .process_at_rank(status.source_rank())
+                .receive_into(&mut buf);
+
+            let eyes_data: EyesData = bincode::deserialize(&buf).unwrap();
+            let mut eyes_detector =
+                objdetect::CascadeClassifier::new(eye_xml.clone().as_str()).unwrap();
+            let EyesData {
+                mut frame,
+                equalized,
+                faces,
+            } = eyes_data;
+            for face in faces {
+                let eyes = common::detect_eyes(
+                    &core::Mat::roi(&equalized.0, unsafe { std::mem::transmute(face) })
+                        .unwrap()
+                        .clone_pointee(),
+                    &mut eyes_detector,
+                )
+                .unwrap();
+                common::draw_in_frame(&mut frame.0, &eyes, &unsafe { std::mem::transmute(face) })
+                    .unwrap();
+            }
+            let mat = MatData(frame.0);
+            {
+                let bytes = bincode::serialize(&mat).unwrap();
+                let size = bytes.len() as u32;
+                let target = sender.process_at_rank(target);
+                target.send(&size.to_ne_bytes());
+                target.send(&bytes);
+            }
+        }
+        let target = sender.process_at_rank(target);
+        target.send(&0u32.to_ne_bytes());
+    }
+
+    drop(universe);
+    std::process::exit(0);
 }
