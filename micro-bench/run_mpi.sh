b9e6b1688688bd4a2357c3c22e5785ae87075a0e
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2024-05-15 15:06:27 -0300

    change run script paramenters for micro-bench

--- micro-bench/run_mpi.sh
+++ micro-bench/run_mpi.sh
@@ -9,9 +9,9 @@ set -e
 
 RUNTIMES="mpi spar-rust-mpi"
 WORKERS=$1
-MD=8192 # 2 ^ 13
-ITER1=5000
-ITER2=4000
+MD=4096 # 2 ^ 12
+ITER1=4000
+ITER2=3000
 
 
 LOG_DIR="log-$(date '+%Y-%m-%d_%H:%M:%S:%N')"
