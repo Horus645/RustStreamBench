a0ff3e4e8bc0e6d521cc09b7114fe503f5392f7c
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2024-06-15 19:20:10 -0300

    plot.py: force efficiency to always go from 0-100

--- plots/plot.py
+++ plots/plot.py
@@ -150,4 +150,6 @@ ax2.legend(loc=0, bbox_to_anchor=(1,1.1645))
 ax1.set_xticks(plot_data["Ideal"][0])
 ax1.set_yticks([1, 5, 10, 15, 20, 25, 30, 35, 40, 45])
 
+ax2.set_yticks([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+
 plt.savefig(graph_name + ".svg", bbox_inches='tight')
