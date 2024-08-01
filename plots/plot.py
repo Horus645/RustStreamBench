df8ae2e43845eacf7bc5456c58339e4243c2378b
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2024-08-01 14:39:08 -0300

    adjust plotting scripts

--- plots/plot.py
+++ plots/plot.py
@@ -3,12 +3,14 @@ import math
 import os
 import matplotlib.pyplot as plt
 
+
 def read_floats(filename):
     file_content = open(sys.argv[1] + "/" + filename, "r")
     floats = []
     for line in file_content.readlines():
         floats.append(float(line))
     return floats
+
 
 def mean(values):
     if len(values) == 0: return 0
@@ -17,6 +19,7 @@ def mean(values):
     for value in values: m += value
     return m / len(values)
 
+
 def stddev(values, mean):
     if len(values) == 0: return 0
 
@@ -24,9 +27,11 @@ def stddev(values, mean):
     for value in values: dev += math.pow(value - mean, 2)
     return math.sqrt(dev / len(values))
 
+
 if len(sys.argv) < 2:
     print("usage: " + sys.argv[0] + " <directory in [data]> <graph name>")
     exit(-1)
+
 
 def export_legend(legend, filename="legend.pdf", expand=[-2,-2,2,2]):
     fig  = legend.figure
@@ -50,9 +55,15 @@ directory = os.fsencode(sys.argv[1])
 
 sequential_mean = 0
 sequential_stddev = 0
+items = 0
+
 graph_data = []
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
+    if filename == "ITEMS":
+        content = open(sys.argv[1] + "/" + filename, 'r')
+        items = int(content.readlines()[0])
+        continue
 
     values = read_floats(filename)
     m = mean(values)
@@ -61,73 +72,31 @@ for file in os.listdir(directory):
     try:
         runtime, threads = filename.split('-')
         if runtime == "SPAR_RUST_MPI":
-            runtime = "Our Abstraction"
+            runtime = "Our Work"
         elif runtime == "MPI":
             runtime = "Raw MPI"
-        graph_data.append((runtime, int(threads), m, dev))
+        graph_data.append((runtime, int(threads) - 1, m, dev))
     except ValueError:
         if filename == "SEQUENTIAL":
             sequential_mean = m
             sequential_stddev = dev
-    
+
 
 graph_data.sort()
 
+seq_throughput = items / sequential_mean
+seq_stddev = seq_throughput - (items / (sequential_mean + sequential_stddev))
 plot_data = {}
-plot_data["Our Abstraction"] = ([1], [1], [sequential_stddev], [100])
-plot_data["Raw MPI"] = ([1], [1], [sequential_stddev], [100])
-plot_data["Ideal"] = ([1], [1], [sequential_stddev])
+plot_data["Our Work"] = ([0], [seq_throughput], [seq_stddev])
+plot_data["Raw MPI"] = ([0], [seq_throughput], [seq_stddev])
 
 for (runtime, thread, mean, stddev) in graph_data:
     plot_data[runtime][0].append(thread)
-    plot_data[runtime][1].append(sequential_mean / mean)
-    plot_data[runtime][2].append(stddev)
-    plot_data[runtime][3].append(100 * ((sequential_mean / mean) / thread))
-
-    if plot_data["Ideal"][0][-1] < thread:
-        plot_data["Ideal"][0].append(thread)
-        plot_data["Ideal"][1].append(thread)
-        plot_data["Ideal"][2].append(0)
+    plot_data[runtime][1].append(items / mean)
+    plot_data[runtime][2].append((items / mean) - (items / (mean + stddev)))
 
 
 fig, ax1 = plt.subplots()
-ax2 = ax1.twinx()
-
-ax1.set_zorder(ax2.get_zorder()+1) # put ax in front of ax2
-ax1.patch.set_visible(False) # hide the 'canvas'
-ax2.patch.set_visible(True) # show the 'canvas'
-
-ax2.bar(
-    [x - 0.5 for x in plot_data["Raw MPI"][0] if x != 1],
-    plot_data["Raw MPI"][3][1:],
-    1.0,
-    label = "Raw MPI",
-    fill = False,
-    color = "crimson",
-    hatch = "//"
-)
-
-ax2.bar(
-    [x + 0.5 for x in plot_data["Our Abstraction"][0] if x != 1],
-    plot_data["Our Abstraction"][3][1:],
-    1.0,
-    label = "Our Abtraction",
-    fill = False,
-    color = "aqua",
-    hatch = ".."
-)
-
-ax1.errorbar(
-    plot_data["Ideal"][0],
-    plot_data["Ideal"][1],
-    plot_data["Ideal"][2],
-    color="blue",
-    label = "Ideal",
-    fmt = '.',
-    elinewidth = 0.01,
-    markersize = 8.0,
-    linestyle = "dotted",
-)
 
 ax1.errorbar(
     plot_data["Raw MPI"][0],
@@ -143,37 +112,35 @@ ax1.errorbar(
 )
 
 ax1.errorbar(
-    plot_data["Our Abstraction"][0],
-    plot_data["Our Abstraction"][1],
-    plot_data["Our Abstraction"][2],
+    plot_data["Our Work"][0],
+    plot_data["Our Work"][1],
+    plot_data["Our Work"][2],
     color="green",
-    label = "Our Abtraction",
+    label = "Our Work",
     fmt = '^',
-    linestyle = "dashdot",
+    linestyle = "dotted",
     markersize = 5.0,
     elinewidth = 0.01,
     capsize = 3,
 )
 
-ax1.set_xlabel("Workers", fontsize=20)
-ax1.set_ylabel("Speedup", fontsize=20)
-legend1 = ax1.legend(loc=0, bbox_to_anchor=(0.335,1.22))
+throughput_label = "Throughput "
+if graph_name == "micro-bench":
+    throughput_label += "(lines/sec)"
+elif graph_name == "eye-detector":
+    throughput_label += "(frames/sec)"
+elif graph_name == "image-processing":
+    throughput_label += "(images/sec)"
+else:
+    throughput_label += "(chunks/sec)"
 
-ax2.set_ylabel("Efficiency", fontsize=20)
-legend2 = ax2.legend(loc=0, bbox_to_anchor=(1,1.1645))
+ax1.set_xlabel("Replicated Stages", fontsize=20)
+ax1.set_ylabel(throughput_label, fontsize=20)
 
-ax1.set_xticks(plot_data["Ideal"][0])
-ax1.set_yticks([1, 5, 10, 15, 20, 25, 30, 35, 40, 45])
+ax1.set_xticks(plot_data["Our Work"][0][1:])
+# ax1.set_yticks([1, 5, 10, 15, 20, 25, 30, 35, 40, 45])
 ax1.tick_params(axis='both', which='major', labelsize=16)
 ax1.tick_params(axis='both', which='minor', labelsize=14)
 
-ax2.set_yticks([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
-ax2.tick_params(axis='both', which='major', labelsize=16)
-ax2.tick_params(axis='both', which='minor', labelsize=14)
-
-export_legend(legend1, "legend1.pdf")
-ax1.get_legend().remove()
-export_legend(legend2, "legend2.pdf")
-ax2.get_legend().remove()
-
+plt.legend(loc="upper left", fontsize=16)
 plt.savefig(graph_name + ".pdf", bbox_inches='tight')
--- plots/plot_all.sh
+++ plots/plot_all.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 for plot in data/bzip2-compression/*; do
-	python plot.py "$plot" "$(basename "$plot")-compression"
-	echo "$(basename "$plot")-compression"
+	python plot.py "$plot" "$(basename "$plot")-compression" \
+	&& echo "$(basename "$plot")-compression" &
 done
 
 for plot in data/bzip2-decompression/*; do
-	python plot.py "$plot" "$(basename "$plot")-decompression"
-	echo "$(basename "$plot")-decompression"
+	python plot.py "$plot" "$(basename "$plot")-decompression" \
+	&& echo "$(basename "$plot")-decompression" &
 done
 
 for plot in micro-bench image-processing eye-detector; do
-	python plot.py data/"$plot" "$plot"
-	echo "$plot"
+	python plot.py data/"$plot" "$plot" \
+	&& echo "$plot" &
 done
+
+wait
